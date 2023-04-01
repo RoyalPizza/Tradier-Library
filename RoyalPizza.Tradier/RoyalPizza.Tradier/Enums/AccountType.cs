9e66ab810348df3db5c64c19e5923b8083d3b6e2
RoyalPizza <68163537+RoyalPizza@users.noreply.github.com>
2023-04-01 11:47:23 -0400

    Ran Cleanup

--- RoyalPizza.Tradier/RoyalPizza.Tradier/Enums/AccountType.cs
+++ RoyalPizza.Tradier/RoyalPizza.Tradier/Enums/AccountType.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 
 namespace RoyalPizza.Tradier.Enums
 {
@@ -23,7 +18,7 @@ namespace RoyalPizza.Tradier.Enums
         /// </summary>
         [Description("Cash")]
         Cash,
-        
+
         /// <summary>
         /// 
         /// </summary>
